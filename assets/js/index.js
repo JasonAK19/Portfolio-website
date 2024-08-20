edc1303d6e1c13100ff4e447f23bf42e342cdda7
Jason Appiah <jasona2@umbc.edu>
2024-08-19 22:11:02 -0400

    course work update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,24 @@
-/*=============== SERVICES MODAL ===============*/
+/*=============== COURSES MODAL ===============*/
 
+const modals = document.querySelectorAll('.course__modal'),
+    modalButtons = document.querySelectorAll('.course__button'),
+    modalCloses = document.querySelectorAll('.course__modal-close');
+
+let activeModal = (modalClick) => {
+    modals[modalClick].classList.add('active-modal');
+}
+
+modalButtons.forEach((modalButton, i) => {
+    modalButton.addEventListener('click', () => { 
+        activeModal(i);
+    });
+});
+
+modalCloses.forEach((modalClose, i) => {
+    modalClose.addEventListener('click', () => {
+        modals[i].classList.remove('active-modal');
+    });
+});
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
